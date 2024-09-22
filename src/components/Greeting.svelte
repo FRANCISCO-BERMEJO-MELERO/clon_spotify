060971a7db5daa956724a95820140d904b68954b
Fran <franbermejo.dam@gmail.com>
2024-09-22 21:04:57 +0200

    Add anination playListCard

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+    const greeting = currentHour < 12 ? 'Buenos días' : currentHour < 18 ? 'Buenas tardes' : 'Buenas noches'
+</script>
+
+<div class="text-white text-3xl font-bold">{greeting}</div>

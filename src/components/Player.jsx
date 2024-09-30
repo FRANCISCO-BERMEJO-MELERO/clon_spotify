3b34d56ad3787a178d3b7bc601416850b89ec7e8
Fran <franbermejo.dam@gmail.com>
2024-09-30 11:51:50 +0200

    actualizamos el player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 export const Pause = ({ className }) => (
     <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
 )
@@ -17,6 +17,18 @@ export const Volume = () => (
 
 export function Player() {
     const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef();
+    useEffect(() => {
+        audioRef.current.src = '/music/1/01.mp3'
+    }, []);    
+    const handleClick = () => {
+        if(isPlaying){
+            audioRef.current.pause();
+        }else{
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    }
     return (
         <div  className=" flex flex-row justify-between w-full px-4 z-50">
             <div>
@@ -24,15 +36,15 @@ export function Player() {
             </div>
             <div className="grid place-content-center gap-4 flex-1">
                 <div className="flex justify-center">
-                    <button className="rounded-full bg-white p-2" onClick={() => setIsPlaying(!isPlaying)}>
+                    <button className="rounded-full bg-white p-2" onClick={handleClick}>
                         {isPlaying ? <Pause /> : <Play />}
                     </button>
                 </div>
-                Reproductor
             </div>
-            <div>
-                Sonido
+            <div className="grid place-content-center">
+                
             </div>
+            <audio ref={audioRef}/>
         </div>
     )
 }
